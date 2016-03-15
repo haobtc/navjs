069ac2881e0d3edf09a8bf58dae0b58c81f8664b
Zeng Ke <superisaac.ke@gmail.com>
2016-03-15 13:06:11 +0800

    Added NavJsSwiftDemo Project

---
+++ iOS/NavJsSwiftDemo/NavJsSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavJsSwiftDemo
+//
+//  Created by ZengKe on 16/3/15.
+//  Copyright © 2016年 Haobtc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
